6af829ca93b27e2f8162260c2171d82004807752
Amy1481 <amishamohanty2025@gmail.com>
2026-01-18 12:24:21 +0530

    Add JS lessons structure

---
+++ OOPS/Assignments/Assignment1/Q1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+enum Day
+{
+    Sunday = 1,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+int
+main()
+{
+    int dayNumber;
+    cout << "Enter day number (1-7): ";
+    cin >> dayNumber;
+    Day day = (Day)dayNumber;
+    switch (day)
+    {
+    case Sunday:
+        cout << "Sunday";
+        break;
+    case Monday:
+        cout << "Monday";
+        break;
+    case Tuesday:
+        cout << "Tuesday";
+        break;
+    case Wednesday:
+        cout << "Wednesday";
+        break;
+    case Thursday:
+        cout << "Thursday";
+        break;
+    case Friday:
+        cout << "Friday";
+        break;
+    case Saturday:
+        cout << "Saturday";
+        break;
+    default:
+        cout << "Invalid day"
+    }
+    return 0;
+}
